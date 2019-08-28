433d356e0fe44836722e9b51bdcf0c78b3181db7
hattallipuneet <hattallipuneet@gmail.com>
2019-08-28 21:06:28 +0530

    Mapping SortedSet value type collection
    Important annotations :
    @OrderBy("[property name] ASC|DEC") // allows sorting by single value only

---
+++ src/main/java/com/entity/sortedmapping/Employee.java
@@ -0,0 +1,75 @@
+package com.entity.sortedmapping;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Table(name = "employee")
+@Entity
+public class Employee {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "salaries")
+    @OrderBy("salary DESC")
+    @Column(name = "salary")
+    private Set<Integer> salaries = new LinkedHashSet<>();
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Integer> getSalaries() {
+        return salaries;
+    }
+
+    public void setSalaries(Set<Integer> salaries) {
+        this.salaries = salaries;
+    }
+}
---
+++ src/test/java/tests/mapping/SortedSetTest.java
@@ -0,0 +1,70 @@
+package tests.mapping;
+
+import com.entity.sortedmapping.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SortedSetTest {
+
+    private static SessionFactory sessionFactory;
+
+    @BeforeClass
+    public static void init() {
+        sessionFactory = new Configuration().configure("hibernate/hibernate-employee-tracker-cfg.xml")
+                                            .addAnnotatedClass(Employee.class)
+                                            .buildSessionFactory();
+    }
+
+    @Test
+    public void test_Insert() {
+        Transaction transaction = null;
+
+        try(Session session = sessionFactory.getCurrentSession()) {
+
+            Employee employee =
+                    new Employee("Chandler", "Bing", "cmbing@friends.com");
+            Set<Integer> salaries = IntStream.of(10000,15000,20000,18000)
+                                            .boxed()
+                                            .collect(Collectors.toCollection(LinkedHashSet::new));
+            employee.setSalaries(salaries);
+            transaction = session.beginTransaction();
+            session.persist(employee);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void test_Select_Return_Sorted_Data() {
+        try(Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Employee rachel = session.get(Employee.class, 102);
+            System.out.println(rachel.getSalaries());
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @AfterClass
+    public static void destroy() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
+}
