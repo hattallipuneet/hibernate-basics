10b6ea93dbb456a26b4e05460e2c56c668f0233d
hattallipuneet <hattallipuneet@gmail.com>
2019-08-28 19:37:27 +0530

    Mapping value type data using Map
    Important annotations:
    
    @MapKeyColumn(name="columnRepresentingMapKey")
    @ElementCollection
    @CollectionTable

---
+++ src/main/java/com/entity/mapmapping/StockHolder.java
@@ -0,0 +1,69 @@
+package com.entity.mapmapping;
+
+import javax.persistence.*;
+import java.util.Map;
+
+@Table
+@Entity(name = "stockholder")
+public class StockHolder {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @ElementCollection
+    @CollectionTable(name = "stocks")
+    @MapKeyColumn(name = "company_code")
+    @Column(name = "company_name")
+    private Map<String, String> assets;
+
+    public StockHolder(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Map<String, String> getAssets() {
+        return assets;
+    }
+
+    public void setAssets(Map<String, String> assets) {
+        this.assets = assets;
+    }
+
+    @Override
+    public String toString() {
+        return "StockHolder{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", assets=" + assets +
+                '}';
+    }
+}
---
+++ src/test/java/mapping/valuetype/MapMappingTest.java
@@ -0,0 +1,60 @@
+package mapping.valuetype;
+
+import com.entity.mapmapping.StockHolder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MapMappingTest {
+
+    private static SessionFactory sessionFactory;
+
+    @BeforeClass
+    public static void init() {
+        sessionFactory = new Configuration().configure("hibernate/hibernate-stock-tracker-cfg.xml")
+                                            .addAnnotatedClass(StockHolder.class)
+                                            .buildSessionFactory();
+    }
+
+    @Test
+    public void test_Insert() {
+        Transaction transaction = null;
+        try(Session session = sessionFactory.getCurrentSession();) {
+
+            Map<String, String> assets = Stream.of(
+                    new AbstractMap.SimpleEntry<>("GOOG", "Google"),
+                    new AbstractMap.SimpleEntry<>("APPL", "Apple"),
+                    new AbstractMap.SimpleEntry<>("MSFT", "Micosoft")
+            ).collect(Collectors.toMap(Map.Entry::getKey,
+                                        Map.Entry::getValue));
+
+            StockHolder stockHolder = new StockHolder("Monica", "Geller");
+            stockHolder.setAssets(assets);
+            transaction = session.beginTransaction();
+            session.persist(stockHolder);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @AfterClass
+    public static void destroy() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
+}
