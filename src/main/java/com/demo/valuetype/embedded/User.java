b40f75583512cfbc343d84b857e87b7033ac3e1c
hattallipuneet <hattallipuneet@gmail.com>
2019-08-30 00:01:19 +0530

    Using @Embeddable to create reusable value type class
    Important annotations :
    @Embeddable
    @Embedded (This is optional, Hibernate will still be able to detect the embeddable component. )

---
+++ src/main/java/com/demo/valuetype/embedded/User.java
@@ -0,0 +1,53 @@
+package com.demo.valuetype.embedded;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Embedded
+    private Contact contact;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+}
